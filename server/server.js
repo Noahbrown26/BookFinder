8bb3cd6388d68ac20b6415ae77cb25939e386cc7
Noahb26 <noahbrown2663@gmail.com>
2022-12-29 17:26:22 -0800

    initialize apollo server in server.js

--- server/server.js
+++ server/server.js
@@ -3,8 +3,17 @@ const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
 
+const { authMiddleware } = require('./utils/auth');
+const { ApolloServer } = require('apollo-server-express');
+const { typeDefs, resolvers } = require('./schemas');
+
 const app = express();
 const PORT = process.env.PORT || 3001;
+const server = new ApolloServer({
+  typeDefs,
+  resolvers, 
+  context: authMiddleware, 
+});
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -16,5 +25,7 @@ app.use(express.static(path.join(__dirname, '../client/build')));
 app.use(routes);
 
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => {
+    console.log(`🌍 Now listening on localhost:${PORT}`); 
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);})
 });

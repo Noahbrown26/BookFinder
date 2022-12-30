d6e81a9d15c378be371355882ff67cef36a3fcfc
Noahb26 <noahbrown2663@gmail.com>
2022-12-29 17:52:07 -0800

    fix apollo connection and schema exporting

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
-const routes = require('./routes');
+//const routes = require('./routes');
 
 const { authMiddleware } = require('./utils/auth');
 const { ApolloServer } = require('apollo-server-express');
@@ -11,9 +11,11 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 const server = new ApolloServer({
   typeDefs,
-  resolvers, 
-  context: authMiddleware, 
+  resolvers,
+  context: authMiddleware
 });
+
+server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -21,11 +23,15 @@ app.use(express.json());
 
 app.use(express.static(path.join(__dirname, '../client/build')));
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
-app.use(routes);
+//app.use(routes);
 
 db.once('open', () => {
   app.listen(PORT, () => {
-    console.log(`🌍 Now listening on localhost:${PORT}`); 
-    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);})
+    console.log(`🌍 Now listening on localhost:${PORT}`);
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  });
 });

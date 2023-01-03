0a0901d42c1bf18160060d8156d01071cde609ad
Noahb26 <noahbrown2663@gmail.com>
2023-01-03 14:32:22 -0800

    update server auth utils & server dependancies

--- server/server.js
+++ server/server.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const path = require('path');
-const db = require('./config/connection');
-//const routes = require('./routes');
-
-const cors = require('cors');
-const { authMiddleware } = require('./utils/auth');
 const { ApolloServer } = require('apollo-server-express');
+const db = require('./config/connection');
+const { authMiddleware } = require('./utils/auth');
 const { typeDefs, resolvers } = require('./schemas');
+const cors = require('cors');
+
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -24,11 +24,11 @@ app.use(cors());
 
 app.use(express.static(path.join(__dirname, '../client/build')));
 
+
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
-//app.use(routes);
 
 db.once('open', () => {
   app.listen(PORT, () => {

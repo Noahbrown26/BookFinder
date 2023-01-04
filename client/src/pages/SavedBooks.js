83fc82f34c081b01f1a5eda3b2ee98b56c2e6a82
Noahb26 <noahbrown2663@gmail.com>
2023-01-03 16:38:24 -0800

    further bug fixing on front-end and backend

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -1,7 +1,5 @@
-
 import React from 'react';
 import { Jumbotron, Container, CardColumns, Card, Button } from 'react-bootstrap';
-
 import { useQuery, useMutation } from '@apollo/react-hooks';
 import { GET_ME } from '../utils/queries';
 import { REMOVE_BOOK } from '../utils/mutations';
@@ -10,46 +8,30 @@ import { removeBookId } from '../utils/localStorage';
 
 const SavedBooks = () => {
   const { loading, data } = useQuery(GET_ME);
-  const [deleteBook] = useMutation(REMOVE_BOOK);
+
   const userData = data?.me || {};
 
-  if(!userData?.username) {
-    return (
-      <h4>
-        You need to be logged in to see this page. Use the navigation links above to sign up or log in!
-      </h4>
-    );
-  }
+  // pass in remove book mutation //
+  const [removeBook] = useMutation(REMOVE_BOOK)
 
-  // create function that accepts the book's mongo _id value as param and deletes the book from the database
-  const handleDeleteBook = async (bookId) => {
-    const token = Auth.loggedIn() ? Auth.getToken() : null;
+
+  const handleRemoveBook = async (bookId) => {
+    const token = Auth.loggedIn() ? Auth.getToken(): null;
 
     if (!token) {
       return false;
     }
-
     try {
-      await deleteBook({
-        variables: {bookId: bookId},
-        update: cache => {
-          const data = cache.readQuery({ query: GET_ME });
-          const userDataCache = data.me;
-          const savedBooksCache = userDataCache.savedBooks;
-          const updatedBookCache = savedBooksCache.filter((book) => book.bookId !== bookId);
-          data.me.savedBooks = updatedBookCache;
-          cache.writeQuery({ query: GET_ME , data: {data: {...data.me.savedBooks}}})
-        }
+      await removeBook({
+        variables: { bookId: bookId }
       });
-      // upon success, remove book's id from localStorage
       removeBookId(bookId);
-    } catch (err) {
+    }catch (err) {
       console.error(err);
     }
   };
-  // if data isn't here yet, say so
   if (loading) {
-    return <h2>LOADING...</h2>;
+    return <h2>Almost There...</h2>;
   }
 
   return (
@@ -73,10 +55,9 @@ const SavedBooks = () => {
                 <Card.Body>
                   <Card.Title>{book.title}</Card.Title>
                   <p className='small'>Authors: {book.authors}</p>
-                  {book.link ? <Card.Text><a href={book.link} target="_blank">More Information on Google Books</a></Card.Text> : null}
                   <Card.Text>{book.description}</Card.Text>
-                  <Button className='btn-block btn-danger' onClick={() => handleDeleteBook(book.bookId)}>
-                    Delete this Book
+                  <Button className='btn-block btn-danger' onClick={() => handleRemoveBook(book.bookId)}>
+                    Delete this Book!
                   </Button>
                 </Card.Body>
               </Card>
--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,64 +1,64 @@
 const { User, Book } = require('../models');
-const { signToken } = require("../utils/auth");
-const { AuthenticationError } = require("apollo-server-express");
+const { AuthenticationError } = require('apollo-server-express');
+const { signToken } = require('../utils/auth');
 
 const resolvers = {
-  Query: {
-    // query to find user //
-    me: async (parent, args, context) => {
-        if (context.user) {
-            const userData = await User.findOne({ _id: context.user._id })
-            .select('-__v -password')
-            return userData;
+    // query to get user //
+    Query: {
+        me: async (parent, args, context) => {
+            if (context.user) {
+                const userData = await User.findOne({ _id: context.user._id })
+                .select('-__v -password')
+                return userData;
+            }
+            throw new AuthenticationError('Not logged in');
         }
-        throw new AuthenticationError('Please login and try again');
+    },
+    Mutation: {
+        // mutation to add a user //
+        addUser: async (parent, args) => {
+            const user = await User.create(args);
+            const token = signToken(user);
+
+            return { token, user };
+        },
+        // mutation to login the user //
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne( { email });
+            if (!user) {
+                throw new AuthenticationError('Incorrect credentials')
+            }
+            const correctPw = await user.isCorrectPassword(password);
+            if(!correctPw) {
+                throw new AuthenticationError('Incorrect credentials')
+            }
+            const token = signToken(user);
+            return { token, user };
+        },
+        // mutation to save a book //
+        saveBook: async (parent, { book }, context) => {
+            if (context.user) {
+                const updatedUser = await User.findOneAndUpdate(
+                    { _id: context.user._id },
+                    { $addToSet: {savedBooks: book} },
+                    { new: true }
+                )
+                return updatedUser;
+            }
+            throw new AuthenticationError('You need to be logged in!')
+        },
+        // mutation to remove a book //
+        removeBook: async (parent, { bookId }, context) => {
+            if (context.user) {
+                const updatedUser = await User.findOneAndUpdate(
+                    {_id: context.user._id},
+                    { $pull: { savedBooks: { bookId: bookId } } },
+                    { new: true }
+                )
+                return updatedUser;
+            }
+        }
     }
-},
-  Mutation: {
-    // mutation to add a user with jswt //
-    addUser: async (parent, args) => {
-      const user = await User.create(args);
-      const token = signToken(user);
-
-      return { token, user };
-  },
-    // mutation to login user given an email and password //
-    login: async (parent, { email, password }) => {
-      const user = await User.findOne( { email });
-      if (!user) {
-          throw new AuthenticationError('Username or Password incorrect')
-      }
-      const correctPw = await user.isCorrectPassword(password);
-      if(!correctPw) {
-          throw new AuthenticationError('Username or Password incorrect')
-      }
-      const token = signToken(user);
-      return { token, user };
-  },
-    // mutation to save a book given arguements and context //
-    saveBook: async (parent, { book }, context) => {
-      if (context.user) {
-          const updatedUser = await User.findOneAndUpdate(
-              { _id: context.user._id },
-              { $addToSet: {savedBooks: book} },
-              { new: true }
-          )
-          return updatedUser;
-      }
-      throw new AuthenticationError('You need to be logged in!')
-  },
-    // mutation to remove a book given arguements and context //
-    removeBook: async (parent, { bookId }, context) => {
-      if (context.user) {
-          const updatedUser = await User.findOneAndUpdate(
-              {_id: context.user._id},
-              { $pull: { savedBooks: { bookId: bookId } } },
-              { new: true }
-          )
-          return updatedUser;
-      }
-  }
-}
-};
-
-module.exports = resolvers;
+  };
+  
+  module.exports = resolvers;
